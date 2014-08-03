b32e6049c580460ffdb7efeeae5e8759179202b9
monicalhamilton <monicalhamilton@gmail.com>
2014-08-03 12:33:38 -0400

    In SolveBioClient: Added more methods.

--- src/main/java/com/solvebio/SolveBioClient.java
+++ src/main/java/com/solvebio/SolveBioClient.java
@@ -61,13 +61,17 @@ public class SolveBioClient {
 
 	public List<DepositoryVersion> getDepositoryVersions(Depository depo) throws AuthenticationException,
 			InvalidRequestException, APIConnectionException, APIException {
-		// TODO test
 		return depo.getDepositoryVersions(apiKey);
 	}
 
 	public DepositoryVersion getDepositoryVersion(String depositoryName, String versionId)
 			throws AuthenticationException, InvalidRequestException, APIConnectionException, APIException {
 		return DepositoryVersion.retrieve(apiKey, depositoryName, versionId);
+	}
+
+	public DepositoryVersion getDepositoryVersion(String depositoryId)
+			throws AuthenticationException, InvalidRequestException, APIConnectionException, APIException {
+		return DepositoryVersion.retrieve(apiKey, depositoryId);
 	}
 
 	// ----------------------------------------
@@ -83,6 +87,11 @@ public class SolveBioClient {
 		// Looks like you need to have a datasetName argument in order to find a unique dataset
 		// Otherwise, you'd be returning a list of datasets
 		return Dataset.retrieve(apiKey, depositoryName, versionId, datasetName);
+	}
+
+	public Dataset getDataset(String datasetId) throws AuthenticationException,
+			InvalidRequestException, APIConnectionException, APIException {
+		return Dataset.retrieve(apiKey, datasetId);
 	}
 	
 }
